048d024dfbb51fa943dfda08c2dbe9ca4d670275
HM CHOI <ktds22@ktdsui-iMac-75.local>
2017-08-18 18:05:29 +0900

    first commit

---
+++ FoodPick/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MyBook
+//
+//  Created by ktds 22 on 2017. 8. 18..
+//  Copyright © 2017년 OliveNetworks, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ FoodPick/Store.swift
@@ -0,0 +1,9 @@
+//
+//  Store.swift
+//  FoodPick
+//
+//  Created by ktds 22 on 2017. 8. 18..
+//  Copyright © 2017년 OliveNetworks, Inc. All rights reserved.
+//
+
+import Foundation
